8682cbb8247a6b47c5acb5505517ab276c811ef2
KwekuDar <kwekudar@outlook.com>
2021-12-04 20:24:32 -0500

    second commit

---
+++ LightHouse/w2/d3/client.js
@@ -0,0 +1,17 @@
+const net = require("net");
+const connect = function () {
+    const conn = net.createConnection({
+      host: '192.168.1.3',
+      port: 50541 ,
+    });
+  
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+    conn.on("connect", () => {
+        console.log('you ded cuz you idled')
+    });
+  
+    return conn;
+  };
+
+  module.exports = connect;
--- LightHouse/w2/d3/play.js
+++ LightHouse/w2/d3/play.js
@@ -1,20 +1,21 @@
+const connect = require('./client')
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.1.3',
-    port: 50541 ,
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '192.168.1.3',
+//     port: 50541 ,
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-      console.log('you ded cuz you idled')
-  });
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+//   conn.on("connect", () => {
+//       console.log('you ded cuz you idled')
+//   });
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
