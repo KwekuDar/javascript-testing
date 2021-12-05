aab51f571592a154f5e7e8eaffd3a9fcef0b881c
KwekuDar <kwekudar@outlook.com>
2021-12-04 22:32:01 -0500

    final commit

--- LightHouse/w2/d3/client.js
+++ LightHouse/w2/d3/client.js
@@ -11,13 +11,12 @@ const connect = function () {
   conn.setEncoding("utf8");
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-});conn.on('connect', () => {
     conn.write("Name: ___");
-  });
+});
   conn.on('data', (data) => {
     console.log(data);
 });
 
   return conn;
 };
-module.exports = connect;
+module.exports = {connect};
