1f99e25ebfc5f10f5fa63a769bf776cc94e456df
KwekuDar <kwekudar@outlook.com>
2021-12-04 21:29:13 -0500

    'update'

--- LightHouse/w2/d3/client.js
+++ LightHouse/w2/d3/client.js
@@ -1,18 +1,23 @@
 const net = require("net");
-const connect = function () {
-    const conn = net.createConnection({
-      host: "localhost",
-      port: 50541 ,
-    });
-  
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-    conn.on('data', () => {
-      // code that does something when the connection is first established
-      console.log('you ded cuz you idled');
-  });
-  
-    return conn;
-  };
 
-  module.exports = connect;
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541 ,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+});conn.on('connect', () => {
+    conn.write("Name: ___");
+  });
+  conn.on('data', (data) => {
+    console.log(data);
+});
+
+  return conn;
+};
+module.exports = connect;
